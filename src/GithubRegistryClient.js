14aa2f8e82f672cf0871a57731b73fed79086dd8
Iuri Matias <iuri.matias@gmail.com>
2018-04-20 19:02:41 -0400

    support repository specified as git://

--- src/GithubRegistryClient.js
+++ src/GithubRegistryClient.js
@@ -70,15 +70,29 @@ class GithubRegistryClient {
 exports.GithubRegistryClient = GithubRegistryClient;
 function extractRepositoryInfo(repository) {
     const parts = repository.split("/");
-    if (parts.length !== 2) {
+    let owner, repo, ref;
+    if (repository.indexOf("git://") >= 0) {
+      let repoParts = parts[4].split("#");
+
+      owner = parts[3];
+      repo = repoParts[0].replace(".git", "");
+      ref = repoParts[1];
+    } else if (parts.length !== 2) {
         throw new Error("Invalid repository name");
+    } else {
+      let repoParts = parts[1].split("#");
+
+      owner = parts[0];
+      repo = repoParts[0];
+      ref = repoParts[1];
     }
-    const repoParts = parts[1].split("#");
+
     const repoInfo = {
-        owner: parts[0],
-        repo: repoParts[0],
-        ref: repoParts[1] || "master"
+        owner: owner,
+        repo: repo,
+        ref: ref || "master"
     };
+
     return repoInfo;
 }
 // | AuthOAuthToken
