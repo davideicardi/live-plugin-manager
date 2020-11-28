ee2400e5e6f4d698593b0e91578cd20bbc604643
Karel Mácha <karel.macha@karlitos.net>
2020-11-28 18:58:08 +0100

    Enhances the IPluginInfo interface with the description property.

--- src/PluginInfo.ts
+++ src/PluginInfo.ts
@@ -2,6 +2,7 @@ export interface IPluginInfo {
 	readonly mainFile: string;
 	readonly location: string;
 	readonly name: string;
+	readonly description: string;
 	readonly version: string;
 	readonly dependencies: { [name: string]: string };
 }
